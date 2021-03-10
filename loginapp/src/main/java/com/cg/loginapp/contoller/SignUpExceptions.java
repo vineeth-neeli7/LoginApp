42f04b5f99cb85f649ed1bb86c674c3c8a08b306
vineeth-neeli7 <49915249+vineeth-neeli7@users.noreply.github.com>
2021-03-10 20:59:17 +0530

    Delete loginapp directory

--- loginapp/src/main/java/com/cg/loginapp/contoller/SignUpExceptions.java
+++
@@ -1,9 +0,0 @@
-package com.cg.loginapp.contoller;
-
-public class SignUpExceptions extends Throwable{
-   
-	public SignUpExceptions(String msg)
-	{
-		super(msg);
-	}
-}
--- loginapp/src/test/java/com/cg/loginapp/LoginappApplicationTests.java
+++
@@ -1,9 +0,0 @@
-package com.cg.loginapp;
-
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class LoginappApplicationTests {
-
-		
-}
